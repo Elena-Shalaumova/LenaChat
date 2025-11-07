acb0993945c72543539d261361ec0c9e3640b45e
Lena <lena.shalaumova26@gmail.com>
2025-11-07 10:50:52 +0300

    edit3

--- app/src/main/java/com/example/easybot/ChatPage.kt
+++ app/src/main/java/com/example/easybot/ChatPage.kt
@@ -1,14 +1,20 @@
 package com.example.easybot
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,11 +27,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Send
+import com.example.easybot.ui.theme.ColorModelMessage
+import com.example.easybot.ui.theme.ColorUserMessage
+import com.example.easybot.ui.theme.Purple80
 
 @Composable
 fun ChatPage(modifier: Modifier = Modifier, viewModel: ChatViewModel) {
@@ -33,7 +43,10 @@ fun ChatPage(modifier: Modifier = Modifier, viewModel: ChatViewModel) {
         modifier = modifier
     ) {
         AppHeader()
-        MessageList(messageList = viewModel.messageList)
+        MessageList(
+            modifier = Modifier.weight(1f),
+            messageList = viewModel.messageList
+        )
         MessageInput(onMessageSend = {
             viewModel.sendMessage(it)
         })
@@ -45,10 +58,67 @@ fun ChatPage(modifier: Modifier = Modifier, viewModel: ChatViewModel) {
 
 @Composable
 fun MessageList(modifier: Modifier=Modifier,messageList : List<MessageModel>) {
-    LazyColumn {
-        items(messageList){
-            Text(text = it.message)
+    if(messageList.isEmpty()){
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                modifier = Modifier.size(60.dp),
+                painter=painterResource(id=R.drawable.baseline_question_answer_24),
+                contentDescription = "Icon",
+                tint = Purple80,
+            )
+            Text(text="Ask me anything", fontSize = 22.sp)
+        }
+    }else{
+        LazyColumn (
+            modifier = modifier,
+            reverseLayout = true
+        ) {
+            items(messageList.reversed()){
+                MessageRow(messageModel = it)
+            }
+        }
     }
+}
+
+@Composable
+fun MessageRow (messageModel: MessageModel) {
+    val isModel = messageModel.role=="model"
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+          modifier = Modifier.fillMaxWidth()
+        ) {
+
+            Box (
+                    modifier = Modifier.align(if(isModel) Alignment.BottomStart else Alignment.BottomEnd)
+                        .padding(
+                            start = if(isModel) 8.dp else 70.dp,
+                            end = if(isModel) 70.dp else 8.dp,
+                            top = 8.dp,
+                            bottom = 8.dp
+                        )
+                        .clip(RoundedCornerShape(48f))
+                        .background(if(isModel) ColorModelMessage else ColorUserMessage)
+                        .padding(16.dp)
+            ) {
+
+                SelectionContainer {
+                    Text(
+                        text = messageModel.message,
+                        fontWeight = FontWeight.W500,
+                        color = Color.White
+                    )
+                }
+
+            }
+        }
+
     }
 
 }
@@ -71,8 +141,11 @@ fun MessageInput(onMessageSend : (String)-> Unit) {
             message = it
         })
         IconButton(onClick = {
-            onMessageSend(message)
-            message = ""
+            if(message.isNotEmpty()) {
+                onMessageSend(message)
+                message = ""
+            }
+
         }) {
             Icon(
                 imageVector = Icons.Default.Send,
--- app/src/main/java/com/example/easybot/ChatViewModel.kt
+++ app/src/main/java/com/example/easybot/ChatViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.launch
 
 class ChatViewModel : ViewModel() {
@@ -20,12 +21,24 @@ class ChatViewModel : ViewModel() {
 
     fun sendMessage(question : String){
         viewModelScope.launch {
-            val chat = generativeModel.startChat()
+            try{
+                val chat = generativeModel.startChat(
+                    history = messageList.map {
+                        content(it.role){text(it.message)}
+                    }.toList()
+                )
 
-            messageList.add(MessageModel(question,"user"))
+                messageList.add(MessageModel(question,"user"))
+                messageList.add(MessageModel("Typing....","model"))
 
-            val response = chat.sendMessage(question)
-            messageList.add(MessageModel(response.text.toString(),"model"))
+                val response = chat.sendMessage(question)
+                messageList.removeLast()
+                messageList.add(MessageModel(response.text.toString(),"model"))
+            }catch (e:Exception){
+                messageList.removeLast()
+                messageList.add(MessageModel("Error: "+e.message.toString(),"model"))
+            }
+
         }
     }
 }
