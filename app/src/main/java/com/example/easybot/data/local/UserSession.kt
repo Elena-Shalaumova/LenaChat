1136044329224bca8a4403330e0cd0ff23bb4bf7
Nikita Romanov <120597472+Elena-Shalaumova@users.noreply.github.com>
2025-12-10 09:38:41 +0300

    5

--- app/src/main/java/com/example/easybot/data/local/UserSession.kt
+++ app/src/main/java/com/example/easybot/data/local/UserSession.kt
@@ -5,8 +5,6 @@ object UserSession {
     var login: String? = null
 
     var apiBaseUrl: String = "http://10.16.77.51:5167/"
-
-    //var apiBaseUrl: String = "http://10.16.77.51:5167/"
     //var apiBaseUrl: String = "http://192.168.3.8:5167/"
     // текущая выбранная модель
     var selectedModel: String? = null
